5d0e9c16931d33d57cfb03359765759397582836
valerierobles <roblesv08@gmail.com>
2023-05-02 13:11:30 -0500

    server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+Server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,10 +19,14 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  })
 }
 
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${Server.graphqlPath}`)
 });
